51b2823cd5919dfba2aa499abd52b1978f5a8fef
ma7moudk3 <mkk28112000@gmail.com>
2022-08-23 17:49:51 +0300

    products P2

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\products;
 use App\Models\sections;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ProductsController extends Controller
 {
@@ -13,57 +12,59 @@ class ProductsController extends Controller
     public function index()
     {
         $sections = sections::all();
-        return view('products.products',compact('sections'));
+        $products = products::all();
+        return view('products.products', compact('sections','products'));
     }
-
 
     public function create()
     {
         //
     }
 
-
     public function store(Request $request)
     {
-        $request->validate([
-            'product_name' => 'required|unique:sections|max:255',
-            'section_id' => 'required',
-        ], [
-            'product_name.required' => 'يرجي ادخال اسم المنتج',
-            'product_name.unique' => 'اسم المنتج مسجل مسبقا',
-        ]);
-
-        products::create([
-            'product_name' => $request->section_name,
-            'description' => $request->description,
-            'created_by' => (Auth::user()->name),
+        Products::create([
+            'Product_name' => $request->Product_name,
             'section_id' => $request->section_id,
-
+            'description' => $request->description,
         ]);
-        session()->flash('Add', 'تم اضافة القسم بنجاح ');
-        return redirect('/sections');
+        session()->flash('Add', 'تم اضافة المنتج بنجاح ');
+        return redirect('/products');
     }
-
 
     public function show(products $products)
     {
         //
     }
 
-
     public function edit(products $products)
     {
         //
     }
 
-
-    public function update(Request $request, products $products)
+    public function update(Request $request)
     {
-        //
+
+        $id = sections::where('section_name', $request->section_name)->first()->id;
+
+        $Products = Products::findOrFail($request->pro_id);
+
+        $Products->update([
+            'Product_name' => $request->Product_name,
+            'description' => $request->description,
+            'section_id' => $id,
+        ]);
+
+        session()->flash('Edit', 'تم تعديل المنتج بنجاح');
+        return back();
+
     }
 
-    public function destroy(products $products)
+    public function destroy(Request $request)
     {
-        //
+        $Products = products::findOrFail($request->pro_id);
+        $Products->delete();
+        session()->flash('delete', 'تم حذف المنتج بنجاح');
+        return back();
     }
 }
--- app/Models/products.php
+++ app/Models/products.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class products extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        "product_name",
+        "description",
+        "created_by",
+        "section_id"
+    ];
+
+    public function section()
+    {
+        return $this->belongsTo('App\Models\sections');
+    }
 }
